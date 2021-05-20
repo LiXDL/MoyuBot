6eb8070eb65acb1fb1d40fa89c965ff8e4b75f66
LiXDL <peterli.uw19@yahoo.com>
2021-05-20 21:50:44 +0800

    Reformat project structure.

--- MoyuBot/Data/Card/CardDBChecker.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('Card.db', isolation_level='IMMEDIATE')
-cur = conn.cursor()
-
-cur.execute('SELECT * FROM CardInfo;')
-
-print(cur.fetchall())
--- MoyuBot/src/plugins/GameResourceManager/data_source.py
+++
@@ -1,8 +0,0 @@
-import aiosqlite
-from pathlib import Path
-
-
-class DataManager:
-
-    def __init__(self, database: Path):
-        self._conn = aiosqlite.connect(database)
